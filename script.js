c9d1b6f7b375f5169feae9aef33c7d435e98ab14
Chay-May <chayenne.mayara@gmail.com>
2022-09-29 21:11:13 -0300

    Exercício off DOM II

---
+++ script.js
@@ -0,0 +1,12 @@
+const adicionaItem = (event) => {
+    const main = document.getElementById('container')
+    const novoItem = document.createElement('article')
+    // const conteudo = document.createTextNode('novoItem')
+    // novoItem.appendChild(conteudo)
+    main.insertAdjacentElement('beforeend',novoItem)
+    novoItem.setAttribute('class', 'item')
+    novoItem.setAttribute('onclick', 'removeItem(event)')
+}
+const removeItem = (event) => {
+    event.target.remove()
+}
